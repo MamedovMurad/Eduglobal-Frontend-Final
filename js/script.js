f9883d03c7c70ed7ac7ae8d8a2c367da6e24cacc
Mamedov Murad <muradvm@code.edu.az>
2019-12-15 20:05:35 +0400

    dropdown correction hover

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ $(".x").click(function(){
     $(this).parent().parent().parent().parent().parent().parent().hide();
 })
 
-$(".search-icon").click(function(e){
+$(".search-icon").hover(function(e){
     e.preventDefault();
   
      $(".form-control").slideToggle();
     
 });
+$(".dropdown").hover(function () {
+    $(this).addClass("show");
+    $(this).find(".dropdown-menu").addClass("show");
+}, function () {
+    $(this).removeClass("show");
+    $(this).find(".dropdown-menu").removeClass("show");
+});
+
 
 /* var navbar= document.querySelector(".navigation");
 var sticky = navbar.offSetTop;
@@ -42,3 +50,39 @@ $(window).scroll(function () {
     touchMove:false
 
 })  */
+if ($("#mostpopular").length) {
+    $(".#mostpopular").slick({
+        infinite: true,
+        prevArrow: '<button class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+        nextArrow: '<button class="slick-next"><i class="fas fa-angle-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+
+    $('.companies-slider').hover(function () {
+        $(this).find(".slick-prev,.slick-next").css("opacity", "1");
+    }, function () {
+        $(this).find(".slick-prev,.slick-next").css("opacity", "");
+    })
+}
